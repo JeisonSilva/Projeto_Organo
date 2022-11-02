f3d975ddbe2af0fb575f48aff1ac9a8498e30a0c
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2022-11-02 17:20:07 -0300

    Criado grupo de profissionais de programacao

--- src/organo/src/App.js
+++ src/organo/src/App.js
@@ -3,13 +3,19 @@ import Formulario from "./Componentes/Formulario";
 
 import './app.css'
 import Organizacao from "./Componentes/Organizacao";
+import Card from "./Componentes/Card";
+import Programacao from "./Componentes/Organizacao/Programacao";
+import Frontend from "./Componentes/Organizacao/Prontend";
 
 function App() {
   return (
     <div className="app">
-      <Cabecalho/>
-      <Formulario/>
-      <Organizacao titulo="Minha Organização"/>
+      <Cabecalho />
+      <Formulario />
+      <Organizacao titulo="Minha Organização">
+        <Programacao titulo="Programação"/>
+        <Frontend titulo="Front End"/>
+      </Organizacao>
     </div>
   );
 }
--- src/organo/src/Componentes/Organizacao/index.js
+++ src/organo/src/Componentes/Organizacao/index.js
@@ -1,10 +1,11 @@
 import './Organizacao.css'
 
-const Organizacao = (props) =>{
+const Organizacao = (props) => {
     return (
         <section className="organizacao">
             <p className='organizacao__titulo'>{props.titulo}</p>
-            <hr/>
+            <hr />
+            {props.children}
         </section>
     )
 }
