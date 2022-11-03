acd04885424de8bd0ad7319bced809b5e4f547f5
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2022-11-03 00:20:53 -0300

    Criado todos os cards

--- src/organo/src/App.js
+++ src/organo/src/App.js
@@ -7,6 +7,10 @@ import Card from "./Componentes/Card";
 import Programacao from "./Componentes/Organizacao/Programacao";
 import Frontend from "./Componentes/Organizacao/Prontend";
 import DataScience from "./Componentes/Organizacao/DataScience";
+import Devops from "./Componentes/Organizacao/Devops";
+import Uxdesign from "./Componentes/Organizacao/Uxdesign";
+import Mobile from "./Componentes/Organizacao/Mobile";
+import InovacaoGestao from "./Componentes/Organizacao/InovavaoGestao";
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
         <Programacao titulo="Programação"/>
         <Frontend titulo="Front End"/>
         <DataScience titulo="Data Sience"/>
+        <Devops titulo="Devops"/>
+        <Uxdesign titulo="UX e Design"/>
+        <Mobile titulo="Mobile"/>
+        <InovacaoGestao titulo="Inovação E Gestão"/>
       </Organizacao>
     </div>
   );
