fc64ab77e7d109420366adeac0eb9a4115dffc11
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2022-11-03 01:29:12 -0300

    Ajuste no titulo da organizacao

--- src/organo/src/Componentes/Organizacao/index.js
+++ src/organo/src/Componentes/Organizacao/index.js
@@ -3,8 +3,10 @@ import './Organizacao.css'
 const Organizacao = (props) => {
     return (
         <section className="organizacao">
-            <p className='organizacao__titulo'>{props.titulo}</p>
-            <hr />
+            <div className='organizacao__titulo'>
+                <p>{props.titulo}</p>
+                <hr />
+            </div>
             {props.children}
         </section>
     )
