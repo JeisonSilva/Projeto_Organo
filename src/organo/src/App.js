c983d7694ec4319609c51206ffde304dcb5009a2
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2022-11-03 00:57:41 -0300

    Excluido componentes sem uso

--- src/organo/src/App.js
+++ src/organo/src/App.js
@@ -3,14 +3,6 @@ import Formulario from "./Componentes/Formulario";
 
 import './app.css'
 import Organizacao from "./Componentes/Organizacao";
-import Card from "./Componentes/Card";
-import Programacao from "./Componentes/Organizacao/Programacao";
-import Frontend from "./Componentes/Organizacao/Prontend";
-import DataScience from "./Componentes/Organizacao/DataScience";
-import Devops from "./Componentes/Organizacao/Devops";
-import Uxdesign from "./Componentes/Organizacao/Uxdesign";
-import Mobile from "./Componentes/Organizacao/Mobile";
-import InovacaoGestao from "./Componentes/Organizacao/InovavaoGestao";
 import Professores from "./Componentes/Organizacao/Professores";
 
 
