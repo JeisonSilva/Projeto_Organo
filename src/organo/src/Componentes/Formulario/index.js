9e1f4060c8dbf9e8a15a0bd5fc504054b1f6b23e
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2022-11-07 23:58:12 -0300

    Incluido limpesa de campo

--- src/organo/src/Componentes/Formulario/index.js
+++ src/organo/src/Componentes/Formulario/index.js
@@ -17,19 +17,19 @@ const Formulario = (props) => {
         }, {
             id: 3,
             value: 'Data Sience'
-        }, 
+        },
         {
             id: 4,
             value: 'Devops'
-        }, 
+        },
         {
             id: 5,
             value: 'UX e Design'
-        }, 
+        },
         {
             id: 6,
             value: 'Mobile'
-        }, 
+        },
         {
             id: 7,
             value: 'Inovação e Gestão'
@@ -41,20 +41,31 @@ const Formulario = (props) => {
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
-    const salvar = (event)=>{
+    const salvar = (event) => {
         event.preventDefault()
         props.onChange({
             nome, cargo, imagem, time
         })
+
+        limparCampos()
+
     }
+
+    const limparCampos = () => {
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
+
     return (
         <form className="formulario" onSubmit={salvar}>
             <p>Preencha os dados para criar o card do colaborador</p>
-            <CampoTexto label="Nome" placeholder="Digite seu nome" value={nome} setValue={valor => setNome(valor)}/>
-            <CampoTexto label="Cargo" placeholder="Digite seu cargo" value={cargo} setValue={valor => setCargo(valor)}/>
-            <CampoTexto label="Imagem" placeholder="Informe o caminho da imagem" valueDefault='./img/imagem.png' value={imagem} setValue={valor => setImagem(valor)}/>
-            <CampoSelecao label="Time" opcoes={opcoes} value={time} setValue={valor => setTime(valor)}/>
-            <Botao value="Criar card" click={event=> salvar(event)}/>
+            <CampoTexto label="Nome" placeholder="Digite seu nome" value={nome} setValue={valor => setNome(valor)} />
+            <CampoTexto label="Cargo" placeholder="Digite seu cargo" value={cargo} setValue={valor => setCargo(valor)} />
+            <CampoTexto label="Imagem" placeholder="Informe o caminho da imagem" valueDefault='./img/imagem.png' value={imagem} setValue={valor => setImagem(valor)} />
+            <CampoSelecao label="Time" opcoes={opcoes} value={time} setValue={valor => setTime(valor)} />
+            <Botao value="Criar card" click={event => salvar(event)} />
         </form>
     )
 }
